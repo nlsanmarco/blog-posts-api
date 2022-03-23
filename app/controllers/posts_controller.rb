7bdc195f3b81908a3473b6b8a22560b7fc0e2801
Nicole Sanmarco <nicole.sanmarco@yahoo.com>
2022-03-23 14:20:55 -0400

    adds posts controller with index function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ApplicationController
   def index
-    response = HTTP.get(https://api.hatchways.io/assessment/blog/posts?tag=tech)
-    posts = response.pars(:json)
-    render json: posts
+    response = HTTP.get("https://api.hatchways.io/assessment/blog/posts?tag=tech").parse(:json)
+    render json: response
   end
 end
