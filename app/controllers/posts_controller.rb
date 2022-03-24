5bd2887c27bf7395883ffbba138c045c2181712e
Nicole Sanmarco <nicole.sanmarco@yahoo.com>
2022-03-24 14:32:14 -0400

    posts index combines post entries for multiple tags

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,9 +14,35 @@ class PostsController < ApplicationController
       render json: response
     elsif params[:tags]
       tags = params[:tags].split(",")
-      # render json: tags
-      response = HTTP.get("https://api.hatchways.io/assessment/blog/posts?tag=#{tags[0]}").parse(:json)
-      render json: response
+
+      posts = []
+      id = {}
+      response = HTTP.get("https://api.hatchways.io/assessment/blog/posts?tag=#{tags[0]}").parse(:json)["posts"]
+      index = 0
+      while index < response.length
+        posts << response[index]
+        next_id = response[index]["id"]
+        id[next_id] = true
+        index += 1
+      end
+
+      index1 = 1
+      while index1 < tags.length
+        response = HTTP.get("https://api.hatchways.io/assessment/blog/posts?tag=#{tags[index1]}").parse(:json)["posts"]
+        index1 += 1
+        index2 = 0
+        while index2 < response.length
+          next_id = response[index2]["id"]
+          if id[next_id]
+            index2 += 1
+          else
+            posts << response[index2]
+            id[next_id] = true
+            index2 += 1
+          end
+        end
+      end
+      render json: posts
     end
   end
 end
